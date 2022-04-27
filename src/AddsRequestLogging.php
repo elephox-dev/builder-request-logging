6d2bb0433b65a0794957cb076a32b47f43aee8c4
Ricardo Boss <contact@ricardoboss.de>
2022-04-27 16:45:37 +0200

    Initial

---
+++ src/AddsRequestLogging.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Builder\RequestLogging;
+
+use Elephox\DI\Contract\ServiceCollection;
+use Elephox\Web\RequestPipelineBuilder;
+
+trait AddsRequestLogging
+{
+    abstract protected function getServices(): ServiceCollection;
+
+    abstract protected function getPipeline(): RequestPipelineBuilder;
+
+    public function addRequestLogging(): void
+    {
+        if ($this->getServices()->has(LoggingMiddleware::class)) {
+            $middleware = $this->getServices()->requireService(LoggingMiddleware::class);
+        } else {
+            $middleware = $this->getServices()->resolver()->instantiate(LoggingMiddleware::class);
+        }
+
+        $this->getPipeline()->push($middleware);
+    }
+}
